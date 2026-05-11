7ffc36a1042afed80948cb9f377667c2483832ca
MartinStefanov77 <martin.stefanov@itrb.org>
2026-05-11 11:39:48 +0300

    Admin logo change implemented

--- app/(payload)/admin/importMap.js
+++ app/(payload)/admin/importMap.js
@@ -21,6 +21,8 @@ import { StrikethroughFeatureClient as StrikethroughFeatureClient_e70f5e05f09f93
 import { UnderlineFeatureClient as UnderlineFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { BoldFeatureClient as BoldFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { ItalicFeatureClient as ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { default as default_1bfa0bd75afddcf0ac9bdb8789864e15 } from '../../../components/payload/AdminIcon.tsx'
+import { default as default_adb19e1b6e4c7ce4f957cb16e2cd1204 } from '../../../components/payload/AdminLogo.tsx'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 /** @type import('payload').ImportMap */
@@ -48,5 +50,7 @@ export const importMap = {
   "@payloadcms/richtext-lexical/client#UnderlineFeatureClient": UnderlineFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   "@payloadcms/richtext-lexical/client#BoldFeatureClient": BoldFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   "@payloadcms/richtext-lexical/client#ItalicFeatureClient": ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "./components/payload/AdminIcon.tsx#default": default_1bfa0bd75afddcf0ac9bdb8789864e15,
+  "./components/payload/AdminLogo.tsx#default": default_adb19e1b6e4c7ce4f957cb16e2cd1204,
   "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
